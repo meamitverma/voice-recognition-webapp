ad17b417221298af0d713ae2a0d1106c8ad93d5e
Amit Verma <amit.verma.22080@gmail.com>
2023-01-28 15:32:50 +0530

    speech-to-text working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,59 @@
-import { useState } from 'react';
-import './App.css';
-import { useSpeechSynthesis } from 'react-speech-kit'
+import { useState } from "react";
+import "./App.css";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { useSpeechRecognition } from "react-speech-kit";
 
 function App() {
-  const [text, setText] = useState('')
-  const {speak} = useSpeechSynthesis()
+  // speech synthesis
+  const [text, setText] = useState("");
+  const { speak } = useSpeechSynthesis();
 
   const handleOnClick = () => {
-    speak({text : text})
-  }
+    speak({ text: text });
+  };
 
+  // Speech-Recognition
+  const [value, setValue] = useState("");
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      setValue(result);
+      setText(result);
+    },
+  });
 
   return (
     <div className="App">
       <header className="App-header">
+        {/* Speech Synthesis */}
         <h1>Text to Speech Converter in React</h1>
-        <textarea className='textAreaStyle' onChange={(e) => {setText(e.target.value)}}></textarea>
-        <button className='buttonStyle' onClick={() => {handleOnClick()}}>Listen</button>
+        <textarea
+          className="textAreaStyle"
+          // value={value}
+          onChange={(e) => {
+            // setValue(e.target.value);
+            setText(e.target.value);
+          }}
+        ></textarea>
+        <button
+          className="buttonStyle"
+          onClick={() => {
+            handleOnClick();
+          }}
+        >
+          Speak
+        </button>
+
+        {/* Speech recognition */}
+        <div>
+          <textarea
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          <button onMouseDown={listen} onMouseUp={stop}>
+            🎤
+          </button>
+          {listening && <div>Go ahead I'm listening</div>}
+        </div>
       </header>
     </div>
   );
